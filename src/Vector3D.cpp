67abb3d54c982ec9aa04ac2e05cd702c395e4636
Nekkiji <72854349+Nekkiji@users.noreply.github.com>
2023-09-14 15:04:54 -0400

    Add tests and fix some bugs

--- src/Vector3D.cpp
+++ src/Vector3D.cpp
@@ -43,17 +43,17 @@ Vector3D & Vector3D::operator^(const Vector3D vector) const
 {
 	return Vector3D(m_y * vector.m_z - m_z * vector.m_y,
 		m_z * vector.m_x - m_x * vector.m_z,
-		m_y * vector.m_x - m_x * vector.m_y);
+		m_x * vector.m_y - m_y * vector.m_x);
 }
 
 float Vector3D::Norm()
 {
-	return sqrt(pow(m_x,2) + pow(m_z, 2) + pow(m_z, 2));
+	return sqrt(pow(m_x,2) + pow(m_y, 2) + pow(m_z, 2));
 }
 
 float Vector3D::Norm2()
 {
-	return pow(m_x, 2) + pow(m_z, 2) + pow(m_z, 2);
+	return pow(m_x, 2) + pow(m_y, 2) + pow(m_z, 2);
 }
 
 void Vector3D::Normalize()
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,13 +11,54 @@ void ofApp::setup(){
 	speed = Vector3D(1, 1);
 
 	primitives.push_back(sphere);
+
+
+	//Tests
+	std::cout << std::endl << "===============================Tests===============================" << std::endl << std::endl;
+
+	std::cout << std::endl << "-----------------Constructeur / Accesseurs / Mutateurs------------------" << std::endl << std::endl;
+	Vector3D vecTest1 = Vector3D();
+	std::cout << "Création Vector3D nul : " + vecTest1.toString() << std::endl;
+	vecTest1[0] = 5;
+	vecTest1[1] = 6;
+	vecTest1[2] = -5;
+	std::cout << "Modification du Vector3D nul en un Vector3D = (5, 6, -5) avec les mutateurs : " + vecTest1.toString() << std::endl;
+	std::cout
+		<< "Print du Vector3D avec accesseurs : ( "
+		+ to_string(vecTest1[0]) + ", " + to_string(vecTest1[1]) + " )"
+		<< std::endl;
+	Vector3D vecTest2 = Vector3D(7, -4, -2.6);
+	std::cout << "Création second Vector3D (7, -4, -2.6) : " + vecTest2.toString() << std::endl;
+
+	std::cout << std::endl << "-----------------Additions / Soustractions------------------" << std::endl << std::endl;
+
+	std::cout << "Addition des deux Vecor3D (attendu (12, 2, -7.6)) : " + (vecTest1 + vecTest2).toString() << std::endl;
+	std::cout << "Soutraction des deux Vector3D (attendu (-2, 10, -2.4)) : " + (vecTest1 - vecTest2).toString() << std::endl;
+
+	std::cout << std::endl << "-----------------Produits Scalaires------------------" << std::endl << std::endl;
+
+	std::cout << "Produit scalaire des deux Vector3D (attendu 24) : " + to_string(vecTest1 * vecTest2) << std::endl;
+	std::cout << "Produit scalaire avec un Vector3D nul (attendu 0) : " + to_string(vecTest1 * Vector3D()) << std::endl;
+	
+	std::cout << std::endl << "-----------------Normalisations------------------" << std::endl << std::endl;
+
+	std::cout << "Norme du premier Vector3D (attendu sqrt(86)) : " + to_string(vecTest1.Norm()) << std::endl;
+	std::cout << "Norme carrée du premier Vector3D (attendu 86) : " + to_string(vecTest1.Norm2()) << std::endl;
+	vecTest1.Normalize();
+	std::cout << "Normalisation de ce Vector3D (attendu (.539, .647, -.539) : " + vecTest1.toString() << std::endl;
+	vecTest1 = Vector3D(5, 6, -5);
+
+	std::cout << std::endl << "-----------------Produits Vectoriels------------------" << std::endl << std::endl;
+
+	std::cout << "Produit Vectoriel des deux Vector3D (attendu ( -35.6, -22, -62)): " + (vecTest1^vecTest2).toString() << std::endl;
+
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 	position += speed;
 	sphere.setPosition(position.v3());
-	std::cout << position.toString() << std::endl;
 }
 
 //--------------------------------------------------------------
