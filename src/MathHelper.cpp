709d81ab669bfd56772748084514106d6471719e
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-09-16 15:18:01 +0200

    Update MathHelper.cpp

--- src/MathHelper.cpp
+++ src/MathHelper.cpp
@@ -8,7 +8,7 @@ integration over time with Euler alorithm (rectangle method) to get velocity fro
 @param v the vector to integrate, time the current time, v0 the initial velocity
 
 @return the vector integrated
-	*/
+*/
 Vector3D integrate(Vector3D v, float time, Vector3D v0) {
 	// rectangle area on one frame using its duration => all frames make sum on mvt duration
 	return v * time + v0;
