5d017695ddc945b983a347be930ece10ada0e326
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-10-09 18:56:02 +0200

    Def des fonctions

---
+++ src/ParticleForceRegistry.cpp
@@ -0,0 +1,18 @@
+#include "Particle.h"
+#include "ParticleForceRegistry.h"
+
+void ParticleForceRegistry::add(Particle* particle, ParticleForceGenerator* Fg)
+{
+}
+
+void ParticleForceRegistry::remove(Particle* particle, ParticleForceGenerator* Fg)
+{
+}
+
+void ParticleForceRegistry::clear()
+{
+}
+
+void ParticleForceRegistry::updateForces(float duration)
+{
+}
--- src/ParticleForceRegistry.h
+++ src/ParticleForceRegistry.h
@@ -1,12 +1,57 @@
 #include "Particle.h"
+#include "ParticleForceGenerator.h"
 
-class ParticleForceGenerator
+class ParticleForceRegistry
 {
 	public:
+
+		/*
+		Definition of the struct ParticleForceRegistration
+
+		particle, particle to which the force is applied
+		Fg, the generator of the force
+		*/
 		struct ParticleForceRegistration
 		{
 			Particle* particle;
 			ParticleForceGenerator* Fg;
 		};
-};
+
+		/*
+		Create the type Registry
+		
+		Dynamic table of ParticleForceRegistration
+		*/
+		typedef std::vector<ParticleForceRegistration> Registry;
+		
+		/*
+		Add a Particle to Registry
+
+		@param particle, the particle to add
+		@param Fg, the Force Generator associated to the particle
+		*/
+		void add(Particle *particle, ParticleForceGenerator *Fg);
+
+		/*
+		Remove a Particle from the Registry
+
+		@param particle, the particle to remove
+		@param Fg, the Force Generator associated to the particle
+		*/
+		void remove(Particle* particle, ParticleForceGenerator* Fg);
+
+		/*
+		Clear the Registry 
+
+		NOTE : the clearance does not delete the particles and the ParticleForceGenerator in it
+		*/
+		void clear();
+		
+		/*
+		Update the force applied to each particle
+
+		@param duration, frame duration when the force applies
+		*/
+		void updateForces(float duration);
+}
 
