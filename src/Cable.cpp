a70b3def30aac33bdcd8456ff5fbd1835e0b307d
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-10-21 01:15:40 +0200

    Comment Cable.cpp

--- src/Cable.cpp
+++ src/Cable.cpp
@@ -16,16 +16,19 @@ Cable::~Cable() { }
 
 void Cable::updateForce(Particle* particle, float duration)
 {
+	/* to determine if the cable is linked to a particle or a point */
 	if (m_attachPoint != nullptr) updateForPoint(particle);
 	else updateForParticle(particle);
 }
 
 void Cable::updateForPoint(Particle* particle)
 {
+	/* create a vector defined by the particle and the point linked by the cable */
 	Vector3D vectorBetweenParticles = *m_attachPoint - particle->getPosition();
 
 	if (vectorBetweenParticles.Norm() > m_length)
 	{
+		/* The distance between the sleep and the current position */
 		float displacement = vectorBetweenParticles.Norm() - m_length;
 
 		// We don't need to calculate ponderated displacement as the point is supposed unmovable
@@ -38,14 +41,17 @@ void Cable::updateForPoint(Particle* particle)
 
 void Cable::updateForParticle(Particle* particle)
 {
+	/* create a vector defined by the particle and the other particle linked by the cable */
 	Vector3D vectorBetweenParticles = m_attachParticle->getPosition() - particle->getPosition();
 	
 	if (vectorBetweenParticles.Norm() > m_length)
 	{
+		/* The distance between the sleep and the current position */
 		float displacement = vectorBetweenParticles.Norm() - m_length;
 		float firstMass = 1 / m_attachParticle->getInverseMass();
 		float secondMass = 1 / particle->getInverseMass();
 
+		/* Computation of both particle displaceemnt */
 		float firstDisplacement = secondMass / (firstMass + secondMass) * displacement;
 		float secondDisplacement = -1 * firstMass / (firstMass + secondMass) * displacement;
 
