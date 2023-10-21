3ab88768cd75a6e2365c9a6ab1760e0a7719df90
Nekkiji <72854349+Nekkiji@users.noreply.github.com>
2023-10-21 13:12:14 -0400

    Add SpringParticleParticle

--- src/SpringParticleParticle.cpp
+++ src/SpringParticleParticle.cpp
@@ -3,18 +3,46 @@
 #include "Vector3D.h"
 
 class SpringParticleParticle : public ParticleForceGenerator {
+	
+private:
+	/* Particle attached */
+	Particle* m_particle;
+
 	/* elasticity constant */
 	float m_k;
 
 	/* length at sleep */
 	float m_l0;
 
+	/* Damping Coefficient */
+	float m_C;
+
+	/* Damping ratio */
+	float m_z;
+
+	/* Oscilation frenquency */
+	float m_w;
+
+public:
 	/*
 	class constructor
 
 	@param elasticity, elasticity constant
 	*/
-	SpringParticleParticle::SpringParticleParticle(float elasticity) : m_k(elasticity) {}
+	SpringParticleParticle::SpringParticleParticle(Particle* particle, float elasticity = 1, float lenght = 10, float C = 0)
+		:
+		m_particle(particle),
+		m_k(elasticity),
+		m_l0(lenght),
+		m_C(C),
+		m_z(0),
+		m_w(0)
+	{}
+
+	/*
+	class desctructor
+	*/
+	SpringParticleParticle::~SpringParticleParticle() { }
 
 	/*
 	update the particle's gravity
@@ -22,8 +50,32 @@ class SpringParticleParticle : public ParticleForceGenerator {
 	@param *particle, the particle to update
 	@param duration, frame duration when the gravity applies
 	*/
-	virtual void updateForce(Particle* particle, float duration) 
+	virtual void updateForce(Particle* particle, float duration)
 	{
-		particle->addForce(m_k * (1 / particle->getInverseMass()));
+		Vector3D direction = (m_particle->getPosition() - particle->getPosition());
+
+		if (!(direction == Vector3D()))
+		{
+
+			float invMass = particle->getInverseMass();
+			float distance = m_l0 - direction.Norm();
+
+			direction.Normalize();
+
+			float velocityProj = particle->getVelocity() * direction;
+
+			m_w = sqrt(invMass * m_k);
+			m_z = m_C * invMass / 2 * m_w;
+
+			float coeff = (-m_w * m_w * distance) - (2 * m_z * m_w * velocityProj);
+
+			/*std::cout << "Direction : " << direction.toString() << std::endl;
+			std::cout << "distance : " << distance << std::endl;
+			std::cout << "Projection : " << velocityProj << std::endl;
+			std::cout << "Force z : " << (direction * coeff * duration).z() << std::endl;
+			std::cout << "Coefficient : " << coeff << std::endl;*/
+
+			particle->addForce(direction * coeff * duration);
+		}
 	}
 };
