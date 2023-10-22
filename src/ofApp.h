9fd4996430e65ca99a328eab7111a42f5f020f94
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-10-23 01:23:37 +0200

    création d'un dossier force

--- src/ofApp.h
+++ src/ofApp.h
@@ -5,9 +5,9 @@
 #include "Vector3D.h"
 #include "Particle.h"
 #include "Tests.h"
-#include "ParticleForceRegistry.h"
-#include "CollisionHandler.h"
-#include "Spindle.h"
+#include "Forces/ParticleForceRegistry.h"
+#include "Collisions/CollisionHandler.h"
+#include "Collisions/Spindle.h"
 #include "iostream"
 
 class ofApp : public ofBaseApp{
