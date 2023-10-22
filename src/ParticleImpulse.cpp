3f7c4455b0e02de69d469da04b7201a3a8158060
Nekkiji <72854349+Nekkiji@users.noreply.github.com>
2023-10-22 17:28:54 -0400

    Add ImpulseForce

---
+++ src/ParticleImpulse.cpp
@@ -0,0 +1,35 @@
+#include "Particle.h"
+#include "ParticleForceGenerator.h"
+#include "Vector3D.h"
+
+class ParticleImpulse : public ParticleForceGenerator {
+
+private:
+	/* Impulsion Force */
+	Vector3D m_impulsion;
+
+public:
+	/*
+	class constructor
+
+	@param impulse, the vector associated to the impulsion's force
+	*/
+	ParticleImpulse::ParticleImpulse(Vector3D impulse)
+		:
+		ParticleForceGenerator(),
+		m_impulsion(impulse)
+	{ }
+
+	ParticleImpulse::~ParticleImpulse() { }
+
+	/*
+	update the particle's gravity
+
+	@param *particle, the particle to update
+	@param duration, frame duration when the gravity applies
+	*/
+	virtual void updateForce(Particle* particle, float duration)
+	{
+		particle->addForce(m_impulsion * (1 / particle->getInverseMass()) * duration);
+	}
+};
