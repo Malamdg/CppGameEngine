6f0254fa0987970a3090c02329b7d901f501111c
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-10-22 19:01:11 +0200

    Comment ParticleFriction.cpp

--- src/ParticleFriction.cpp
+++ src/ParticleFriction.cpp
@@ -20,16 +20,18 @@ class ParticleFriction : public ParticleForceGenerator {
 	ParticleFriction::ParticleFriction(float k1, float k2) : m_k1(k1), m_k2(k2) {}
 
 	/*
-	update the particle's gravity
+	update the particle's force
 
 	@param *particle, the particle to update
-	@param duration, frame duration when the gravity applies
+	@param duration, frame duration when the friction applies
 	*/
 	virtual void updateForce(Particle* particle, float duration) {
 		m_velocity = particle->getVelocity();
 		float norm_v = m_velocity.Norm();
+		/* Computation of the velocity-dependant coeff according to the given formula */
 		float coeff = m_k1 * norm_v + m_k2*norm_v*norm_v;
 		Vector3D direction = m_velocity;
+		/* Force director vector */
 		direction.Normalize();
 		particle->addForce(direction*coeff);
 	}
