97b1a6b9f8dbfc215541a464aec182dff5efc43a
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-10-22 18:58:31 +0200

    Comment SpringParticlePoint.cpp

--- src/SpringParticlePoint.cpp
+++ src/SpringParticlePoint.cpp
@@ -45,10 +45,10 @@ public:
 	SpringParticlePoint::~SpringParticlePoint() { }
 
 	/*
-	update the particle's gravity
+	update the particle's force
 
 	@param *particle, the particle to update
-	@param duration, frame duration when the gravity applies
+	@param duration, frame duration when the srping's force applies
 	*/
 	virtual void updateForce(Particle* particle, float duration) 
 	{
