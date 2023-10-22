390e5f1878cacd41b60df3adb3ea0ebb074d3a94
Nekkiji <72854349+Nekkiji@users.noreply.github.com>
2023-10-22 17:48:09 -0400

    Add ParticleDeplacement
    Used for deplacement with inputs

---
+++ src/ParticleDeplacement.cpp
@@ -0,0 +1,39 @@
+#include "Particle.h"
+#include "ParticleForceGenerator.h"
+#include "Vector3D.h"
+
+class ParticleDeplacement : public ParticleForceGenerator {
+
+private:
+
+	/* Force's Deplacement*/
+	Vector3D m_force;
+
+public:
+	/*
+	class constructor
+
+	@param deplacementForce, value of the force's deplacement
+	*/
+	ParticleDeplacement::ParticleDeplacement(float deplacementForce)
+		:
+		ParticleForceGenerator(),
+		m_force(Vector3D(deplacementForce))
+	{ }
+
+	ParticleDeplacement::~ParticleDeplacement() { }
+
+	/*
+	update the particle's gravity
+
+	@param *particle, the particle to update
+	@param duration, frame duration when the gravity applies
+	*/
+	virtual void updateForce(Particle* particle, float duration)
+	{
+		if (particle->getInverseMass() != 0)
+		{
+			particle->addForce(m_force * (1 / particle->getInverseMass()));
+		}
+	}
+};
