d4481219bb6a2f136789106f9853a16f75a25d84
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-10-25 04:38:05 +0200

    Comment Collision.h

--- src/Collisions/Collision.h
+++ src/Collisions/Collision.h
@@ -5,9 +5,19 @@
 class Collision
 {
 public:
+	/*
+ 	Class Constructor
+ 	*/
 	Collision();
 
+	/*
+ 	Class Destructor
+ 	*/
 	~Collision();
 
+	/*
+ 	To update the particle's behaviour when a collision occurs
+  	@param particle, the particle to update
+  	*/
 	virtual void update(Particle* particle);
 };
