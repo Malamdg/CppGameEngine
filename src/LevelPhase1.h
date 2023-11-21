547b0f79d9a217bfa32d7cf306706aae3aba76de
Malam <60240119+Malamdg@users.noreply.github.com>
2023-11-21 14:05:21 -0500

    wip getting files

---
+++ src/LevelPhase1.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "Level.h"
+#include "of3dPrimitives.h"
+#include "DataStructures/Vector3D.h"
+#include "Particle.h"
+
+class LevelPhase1 : public Level
+{
+public:
+	string name;
+
+	LevelPhase1();
+	~LevelPhase1();
+
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+private:
+
+	//Camera
+	ofCamera cam;
+
+	//**Primitives**//
+	std::list<std::pair<of3dPrimitive*, int*>> primitives;
+	std::list<Particle*> particles;
+	std::list<std::pair<ofSpherePrimitive*, int*>> preview;
+
+	Vector3D m_initialPosition = Vector3D();
+	ofBoxPrimitive floor;
+	ofSpherePrimitive particleVisualization;
+
+	//Colors & Textures
+	Vector3D* colors = new Vector3D[5];
+	Vector3D visualizationColor = Vector3D(0, 255, 0);
+
+	ofTexture* textures = new ofTexture[2];
+	ofTexture textureVisualization;
+
+	// Shooting modes
+	int mode = 0;
+
+	// Constants 
+	float gravity = 9.8;
+
+	// Texts to display
+	string commandText;
+
+	/*
+	* Function to get launch direction from mouse
+	*
+	* @param float x, mouse's x on screen
+	* @param float y, mouse's y on screen
+	* @return Vector3D, normalized shooting direction vector
+	*/
+	Vector3D GetLaunchDirection(float x, float y);
+
+	/*
+	* Method to display a previsualization of current shot
+	*
+	* @param Vector3D initialPosition, particle's starting point from (0, 0, 0)
+	*/
+	void GeneratePrevisualization(Vector3D initialPosition = Vector3D());
+
+};
