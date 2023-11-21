a1f194b8c3b1c53f2ca4a68df7e149c966a48a5f
Malam <60240119+Malamdg@users.noreply.github.com>
2023-11-21 14:09:01 -0500

    files back to project up to date with phase-3

---
+++ src/Level.cpp
@@ -0,0 +1,20 @@
+#include "Level.h"
+
+Level::Level() {}
+Level::~Level() {}
+
+void Level::setup() {}
+void Level::update() {}
+void Level::draw() {}
+
+void Level::keyPressed(int key) {}
+void Level::keyReleased(int key) {}
+void Level::mouseMoved(int x, int y) {}
+void Level::mouseDragged(int x, int y, int button) {}
+void Level::mousePressed(int x, int y, int button) {}
+void Level::mouseReleased(int x, int y, int button) {}
+void Level::mouseEntered(int x, int y) {}
+void Level::mouseExited(int x, int y) {}
+void Level::windowResized(int w, int h) {}
+void Level::dragEvent(ofDragInfo dragInfo) {}
+void Level::gotMessage(ofMessage msg) {}
