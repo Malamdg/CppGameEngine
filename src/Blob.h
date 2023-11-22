57d112907f69b457a72f8605754ef24dfdf29123
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-11-22 04:47:05 +0100

    Update comment Blob.h

--- src/Blob.h
+++ src/Blob.h
@@ -18,46 +18,51 @@ private:
 public:
 	std::list<Particle*> m_particles;
 
-    /*
-    class constructor
+    	/*
+    	class constructor
 
 	@param core, the particle at the centre of the blob
    	*/
 	Blob(Particle* core = new Particle());
 
-    /*
+    	/*
    	class destructor
 	*/
 	~Blob();
 
 	/*
-    Get the core of the blob
+   	Get the core of the blob
 
-    @return a pointer to the core of the blob
-    */
+    	@return a pointer to the core of the blob
+    	*/
 	Particle* getCore();
 
 	/*
 	Split the particles from the blob
-    */
+    	*/
 	void split();
 	
 	/*
 	Merge the particle in the blob
 
 	@param particle, the particle to add to the blob
-    */
+    	*/
 	void merge(Particle* particle, ForceRegistry* forceRegistry, CollisionHandler* collisionHandler);
         
-    /*
-    Link all particles in the blob to the core
+    	/*
+    	Link all particles in the blob to the core
 
-    @param forceRegistry, to create a link
-    */
+   	@param forceRegistry, to create a link
+    	@param collisionHandler, to manage collisions
+    	*/
 	void linkParticles(ForceRegistry* forceRegistry, CollisionHandler* collisionHandler);
 	
 	/*
 	* Link a particle to the core of the blob
+
+  	@param particle, the particle to link
+   	@param forceRegistry, to create a link
+    	@param collisionHandler, to manage collisions
 	*/
 	void linkParticle(Particle* particle, ForceRegistry* forceRegistry, CollisionHandler* collisionHandler);
 };
