30a26e87a00f2c3efe3ddc4282f3cd9ea1e9ecc9
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-12-08 03:30:07 +0100

    Comment Contact.h

--- src/Collisions/Contact.h
+++ src/Collisions/Contact.h
@@ -15,25 +15,92 @@ private:
 	float m_restitution;
 	float m_friction;
 
+	/*
+ 	To compute the coefficient of Restitution
+
+ 	@return the coefficient
+  	*/
 	float calculateRestitution();
+
+	/*
+ 	To compute the coefficient of Friction
+
+ 	@return the coefficient
+  	*/
 	float calculateFriction();
 
 public:
-
+	/*
+ 	Empty Constructor
+  	*/
 	Contact();
+	/*
+ 	Class Constructor
+
+  	@param contactPosition, the position of the contact
+   	@param normalContact, the normal of the contact
+    	@param penetration, the depth of the penetration
+     	@param body1, the first RigidBody implied
+      	@param body2, the second RigidBody implied
+  	*/
 	Contact(Vector3D* contactPosition,
 	Vector3D* normalContact,
 	float penetration,
 	RigidBody* body1,
 	RigidBody* body2);
+	/*
+ 	Class Destructor
+  	*/
 	~Contact();
 
+	/*
+ 	To know if the contact happened
+
+	@return true if the contact happened
+  	*/
 	bool hasContact() const;
+
+	/*
+ 	To get the contact point
+
+	@return the coordinate of the contact point
+  	*/
 	Vector3D* getContactPoint() const;
+
+	/*
+ 	To get the normal of the contact
+
+	@return the normal vector
+  	*/
 	Vector3D* getNormal() const;
+
+	/*
+ 	To get the penetration depth
+
+	@return the depth
+  	*/
 	float getPenetration() const;
+
+	/*
+ 	To get the i-th RigidBody implied
+
+ 	@param i, the number of the RigidBody to get
+	@return the i-th RigidBody
+  	*/
 	RigidBody* getBody(int i = 0) const;
+
+	/*
+ 	To get the restitution coefficient
+
+	@return the coefficient
+  	*/
 	float getRestitution() const;
+
+	/*
+ 	To get the friction coefficient
+
+	@return the coefficient
+  	*/
 	float getFriction() const;
 
 };
