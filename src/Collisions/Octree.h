7aaa94e795b439115dd14bcd877af1bd0d0bb2f7
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-12-08 02:51:37 +0100

    Update Octree.h

--- src/Collisions/Octree.h
+++ src/Collisions/Octree.h
@@ -7,6 +7,7 @@
 struct OctreeNode {
     Vector3D min, max;
     list<RigidBody*> rbs;
+    // There is 8 children because we are in 3D
     OctreeNode* children[8];
 
     OctreeNode(const Vector3D& min, const Vector3D& max) : min(min), max(max) {
@@ -22,20 +23,49 @@ private:
     int m_maxCount = 1;
 
 public:
+    /*
+    Empty Constructor
+    */
     Octree() : m_root(nullptr) {}
+
+    /*
+    Class Constructor
+
+    @param min, the initial lower boundary of the octree
+    @param max, the initial upper boundary of the octree
+    */
     Octree(const Vector3D& min, const Vector3D& max) : m_root(new OctreeNode(min, max)) {}
 
-    // Insert a point into the octree
+    /*
+    To insert a RigidBody in the Octree
+
+    @param rb, the RigidBody to add
+    */
     void insert(RigidBody* rb);
 
+    /*
+    To draw the octree
+    */
     void draw();
 
 private:
     OctreeNode* m_root;
 
-    // Recursive function to insert a point into the octree
+    /*
+    Recursive function to insert a point into the octree
+
+    @param node, the node where the RigidBody is added
+    @param rb, the RigidBody to add
+    */
     void insertRecursive(OctreeNode* node, RigidBody* rb);
 
+    /*
+    Function to determine if a RigidBody is in a node
+
+    @param node, the node checked
+    @param rb, the RigidBody to check
+    @return true if the rigidbody is in the node
+    */
     bool rigidBodyInNode(OctreeNode* node, const RigidBody* rb);
 
     // Split a node into eight children
