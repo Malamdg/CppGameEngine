18ea343b60540276ac2da5265dac7764d1d5cc0d
Justin Jourdant <118277925+XordNeuville@users.noreply.github.com>
2023-12-13 06:24:31 +0100

    Update comment RBCollisionHandler.h

--- src/Collisions/RBCollisionHandler.h
+++ src/Collisions/RBCollisionHandler.h
@@ -36,7 +36,7 @@ public :
 	typedef list<GameObject*> Registry;
 
 	/*
-	Constructor
+	Empty Constructor
 	*/
 	RBCollisionHandler();
 
@@ -62,21 +62,28 @@ public :
 	void broadPhase(float duration, ForceRegistry* forceRegistry);
 
 	/*
-	To manage narrow collision with bounding boxes
+	To manage narrow collision with bounding boxes associated to GameObject
 
 	@param duration, frame duration
 	@param forceRegistry, to add the forces generated
+	@param go1, the first gameobject
+ 	@param go2, the second gameobject
 	*/
 	void narrowPhase(float duration, ForceRegistry* forceRegistry, GameObject* go1, GameObject* go2);
 
 	/*
-	To create a Registry from a box of the octree
+	To add a collision to the collectionregistration
 
-	@param rb1, the first GameObject
-	@param rb2, the second GameObject
+	@param contact, the contact to add
 	*/
 	void addcollision(Contact* contact);
 
+	/*
+	to compute the collision (force applied and position update)
+ 
+  	@param duration, frame duration
+     	@param forceRegistry, to add the forces generated
+  	*/
 	void computeCollision(float duration, ForceRegistry* forceRegistry);
 
 private:
